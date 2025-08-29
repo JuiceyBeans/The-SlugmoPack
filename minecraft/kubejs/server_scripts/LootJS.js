511852edbfdf5421bb30573ec7c74fd3c98bf10d
JuiceyBeans <75553966+JuiceyBeans@users.noreply.github.com>
2025-08-29 16:10:42 +0400

    Remove some spawn bonus chest loot

--- minecraft/kubejs/server_scripts/LootJS.js
+++ minecraft/kubejs/server_scripts/LootJS.js
@@ -4,6 +4,8 @@ LootJS.modifiers(event => {
     event.addLootTableModifier("minecraft:chests/ruined_portal").removeLoot("vintagedelight:ghost_pepper")
     event.addLootTableModifier("minecraft:chests/ruined_portal").removeLoot("vintagedelight:ghost_pepper_seeds")
     event.addLootTableModifier("minecraft:chests/ruined_portal").removeLoot("enigmaticlegacy:enigmatic_eye")
+    event.addLootTableModifier("minecraft:chests/spawn_bonus_chest").removeLoot("enigmaticlegacy:enigmatic_eye")
+    event.addLootTableModifier("minecraft:chests/spawn_bonus_chest").removeLoot("sophisticatedbackpacks:backpack")
 
     // const modifiers = event.getGlobalModifiers();
     // modifiers.forEach((modifier) => {
