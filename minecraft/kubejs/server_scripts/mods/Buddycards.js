e050f982c7b7581e96e10d67a73d155aa55061cc
JuiceyBeans <75553966+JuiceyBeans@users.noreply.github.com>
2025-09-05 19:45:54 +0100

    Buddysteel recipe

---
+++ minecraft/kubejs/server_scripts/mods/Buddycards.js
@@ -0,0 +1,9 @@
+ServerEvents.recipes(e => {
+    e.shapeless('buddycards:buddysteel_blend', [
+        'minecraft:iron_ingot',
+        'minecraft:lapis_lazuli',
+        '#buddycards:buddycards',
+        '#buddycards:buddycards',
+        '#buddycards:buddycards'
+    ])
+})
